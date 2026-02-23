122fe50ef8e94565636d6a60559bf3df39e49f93
gurbakshvirk <lovyvirk34@gmail.com>
2026-02-23 12:15:25 +0530

    careted whole product backend

---
+++ react-api-integration06/backend/models/Productmodal.js
@@ -0,0 +1,33 @@
+const mongoose = require('mongoose')
+
+
+const productSchema = new mongoose.Schema({
+
+    
+
+    name:{
+        type:String,
+        required:true,
+        trim:true,
+    },
+    price:{
+        type:Number,
+        required:true,
+    },
+    category:{
+        type:String,
+        required:true,
+        trim:true,
+    },
+    isPopular:{
+        type:Boolean,
+        default:true,
+    },
+    isAvailable:{
+        type:Boolean,
+        default:true,
+    },
+},{timestamps:true});
+
+
+module.exports = mongoose.model("Product" , productSchema)
